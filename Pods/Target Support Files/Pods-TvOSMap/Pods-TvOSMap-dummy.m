29ab4872f4f4a3efd245ad12aad7f41a9471fcff
Rainblower <red5757687@bk.ru>
2019-06-22 09:44:47 +0300

    Init

---
+++ Pods/Target Support Files/Pods-TvOSMap/Pods-TvOSMap-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TvOSMap : NSObject
+@end
+@implementation PodsDummy_Pods_TvOSMap
+@end
---
+++ Pods/Target Support Files/Pods-TvOSMapUITests/Pods-TvOSMapUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TvOSMapUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TvOSMapUITests
+@end
